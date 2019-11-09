54f516bf0a627654747626b4857467f9aec211cf
Oleg Kovalov <iamolegkovalov@gmail.com>
2019-11-09 21:34:35 +0100

    add Upgrader

---
+++ sse.go
@@ -0,0 +1,59 @@
+package sse
+
+import (
+	"net/http"
+	"time"
+)
+
+var defaultUpgrader Upgrader
+
+func UpgradeHTTP(r *http.Request, w http.ResponseWriter) (*Stream, error) {
+	return defaultUpgrader.UpgradeHTTP(r, w)
+}
+
+var noDeadline time.Time
+
+type Upgrader struct {
+	Timeout time.Duration
+}
+
+func (u Upgrader) UpgradeHTTP(r *http.Request, w http.ResponseWriter) (*Stream, error) {
+	fl, ok := w.(http.Flusher)
+	if !ok {
+		http.Error(w, "Flushing not supported", http.StatusNotImplemented)
+		return nil, ErrNotFlusher
+	}
+
+	hj, ok := w.(http.Hijacker)
+	if !ok {
+		httpError(w, ErrNotHijacker.Error(), http.StatusInternalServerError)
+		return nil, ErrNotHijacker
+	}
+
+	conn, _, err := hj.Hijack()
+	if err != nil {
+		httpError(w, err.Error(), http.StatusInternalServerError)
+		return nil, err
+	}
+
+	// Clear deadlines set by server.
+	conn.SetDeadline(noDeadline)
+	if u.Timeout != 0 {
+		conn.SetWriteDeadline(time.Now().Add(u.Timeout))
+		defer conn.SetWriteDeadline(noDeadline)
+	}
+
+	h := w.Header()
+	h.Set("Cache-Control", "no-cache")
+	h.Set("Connection", "keep-alive")
+	h.Set("Content-Type", "text/event-stream")
+
+	fl.Flush() // flush headers
+
+	s := &Stream{
+		conn:    conn,
+		w:       w,
+		flusher: fl,
+	}
+	return s, nil
+}
